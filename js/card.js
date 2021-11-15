ce1591d8dc6e0d113ecd7ab66027e119d13d368b
Alina <ms.sciurus@gmail.com>
2021-11-15 09:41:29 +0300

    Correct full paint

--- js/card.js
+++ js/card.js
@@ -8,7 +8,7 @@ const fillFeatures = (featuresElement, featuresArray) => {
   featuresArray.forEach((item) => {
     const liElement = document.createElement('li');
 
-    liElement.classList.add('popup__feature' , `popup__feature—-${item}` );
+    liElement.classList.add('popup__feature' , `popup__feature--${item}` );
     featuresElement.appendChild(liElement);
   });
   return fillFeatures;
@@ -21,6 +21,10 @@ const fillPhotos = (photosElement, photosArray) => {
     const imgElement = document.createElement('img');
 
     imgElement.src=`${item}`;
+    imgElement.width='45';
+    imgElement.height='40';
+    imgElement.alt='Фотография жилья';
+
     photosElement.appendChild(imgElement);
   });
 };
@@ -33,13 +37,21 @@ const createAdvertisiments = makeSimilarAdvertisements();
 
 const similarListFragment = document.createDocumentFragment();
 
+const apartmentType  = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель',
+};
+
 createAdvertisiments.forEach((item) => {
   const cardElement = templateAdvertisements.cloneNode(true);
   cardElement.querySelector('.popup__title').textContent = item.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = item.offer.address;
   cardElement.querySelector('.popup__text--price').textContent = `${item.offer.price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = item.offer.type;
-  cardElement.querySelector('.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.guests} гостей`;
+  cardElement.querySelector('.popup__type').textContent = apartmentType [item.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = `${item.offer.rooms} комнаты для ${item.offer.quest} гостей`;
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${item.offer.checkin} , выезд до  ${item.offer.checkout}`;
   fillFeatures(cardElement.querySelector('.popup__features'), item.offer.features);
   cardElement.querySelector('.popup__description').textContent = item.offer.description;
